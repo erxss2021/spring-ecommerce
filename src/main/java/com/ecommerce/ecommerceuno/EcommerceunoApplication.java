75f099571a824c47f46e7fd34f336e3b6165766a
Eloy Sanchez <salmoranse2016@gmail.com>
2024-03-01 12:00:18 -0600

    Creación de la estructura del proyecto y las clases model

---
+++ src/main/java/com/ecommerce/ecommerceuno/EcommerceunoApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.ecommerceuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class EcommerceunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecommerceuno/EcommerceunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerceuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
